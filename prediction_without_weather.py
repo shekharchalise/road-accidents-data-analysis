5534abe222e430bf97bca36636ef5598a7795d4b
shekharchalise <chalise.shekhar@gmail.com>
2020-04-17 00:45:23 -0500

    added without weather condition as added features in predictions

--- prediction_without_weather.py
+++ prediction_without_weather.py
@@ -15,19 +15,23 @@ import os
 import shutil
 
 def clear_output_files():
-    if os.path.exists('./results'):
-       shutil.rmtree('./results')
+    if os.path.exists('./results-without-weather'):
+       shutil.rmtree('./results-without-weather')
     try:
-        os.makedirs('./results/confusion matrix')
-        os.makedirs('./results/classifiction report')
+        os.makedirs('./results-without-weather/confusion matrix')
+        os.makedirs('./results-without-weather/classifiction report')
     except OSError:
         pass
 
 clear_output_files()
 
 input_dataframe = pd.read_csv('preprocessed-file.csv')
+# not using following columns
+#"Temperature(F)","Wind_Chill(F)","Humidity(%)","Pressure(in)","Visibility(mi)","Wind_Speed(mph)","Precipitation(in)","Weather_Condition",
+#input_dataframe= input_dataframe[:5000] #keep 5000
+input_dataframe= input_dataframe[["Severity","Start_Lat","Start_Lng","Zipcode","State","Amenity","Bump","Crossing","Give_Way","Junction","No_Exit","Railway","Roundabout","Station","Stop","Traffic_Calming","Traffic_Signal","Turning_Loop","Start_Time_Year","Start_Time_Month","Start_Time_Day","Start_Time_Hour","Start_Time_Minute","End_Time_Year","End_Time_Month","End_Time_Day","End_Time_Hour","End_Time_Minute"]]
 
-input_dataframe= input_dataframe[:5000] #keep 5000
+
 Y = input_dataframe.Severity.values
 
 cols = input_dataframe.shape[1]
@@ -44,7 +48,7 @@ acc_svc = round(svc.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
 plt.savefig('results/confusion matrix/SVC.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/SVC.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/SVC.csv')
 print(acc_svc)
 
 #KNN
@@ -53,9 +57,9 @@ knn.fit(X_train, Y_train)
 Y_pred = knn.predict(X_test)
 acc_knn = round(knn.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/knn.png')
+plt.savefig('results-without-weather/confusion matrix/knn.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/knn.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/knn.csv')
 print(acc_knn)
 
 # Logistic Regression
@@ -64,65 +68,70 @@ logreg.fit(X_train, Y_train)
 Y_pred = logreg.predict(X_test)
 acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/logistic_regression.png')
+plt.savefig('results-without-weather/confusion matrix/logistic_regression.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/logistic_regression.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/logistic_regression.csv')
 print(acc_log)
 
 # # Gaussian Naive Bayes
+
 gaussian = GaussianNB()
 gaussian.fit(X_train, Y_train)
 Y_pred = gaussian.predict(X_test)
 acc_gaussian = round(gaussian.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/gaussian_nb.png')
+plt.savefig('results-without-weather/confusion matrix/gaussian_nb.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/gaussian_nb.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/gaussian_nb.csv')
 print(acc_gaussian)
 
 # # Perceptron
+
 perceptron = Perceptron()
 perceptron.fit(X_train, Y_train)
 Y_pred = perceptron.predict(X_test)
 acc_perceptron = round(perceptron.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/perceptron.png')
+plt.savefig('results-without-weather/confusion matrix/perceptron.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/perceptron.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/perceptron.csv')
 print(acc_perceptron)
 
 # Stochastic Gradient Descent
+
 sgd = SGDClassifier(max_iter=1200000)
 sgd.fit(X_train, Y_train)
 Y_pred = sgd.predict(X_test)
 acc_sgd = round(sgd.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/SGD.png')
+plt.savefig('results-without-weather/confusion matrix/SGD.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/SGD.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/SGD.csv')
 print(acc_sgd)
 
 # Decision Tree
+
 decision_tree = DecisionTreeClassifier()
 decision_tree.fit(X_train, Y_train)
 Y_pred = decision_tree.predict(X_test)
 acc_decision_tree = round(decision_tree.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/decision_tree.png')
+plt.savefig('results-without-weather/confusion matrix/decision_tree.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/decision_tree.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/decision_tree.csv')
 print(acc_decision_tree)
 
 # # Random Forest
+
 random_forest = RandomForestClassifier(n_estimators=100, random_state = 4)
 random_forest.fit(X_train, Y_train)
 Y_pred = random_forest.predict(X_test)
 random_forest.score(X_train, Y_train)
 acc_random_forest = round(random_forest.score(X_test, Y_test) * 100, 2)
 sns.heatmap(confusion_matrix(Y_test, Y_pred),annot=True,fmt="d") 
-plt.savefig('results/confusion matrix/random_forest.png')
+plt.savefig('results-without-weather/confusion matrix/random_forest.png')
 plt.clf()
-pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results/classifiction report/random_forest.csv')
+pd.DataFrame(classification_report(Y_test,Y_pred, output_dict=True)).transpose().to_csv('results-without-weather/classifiction report/random_forest.csv')
 print(acc_random_forest)
 
 
@@ -134,6 +143,6 @@ models = pd.DataFrame({
     'Score': [acc_svc, acc_knn, acc_log, 
               acc_random_forest, acc_gaussian, acc_perceptron, 
               acc_sgd, acc_decision_tree]})
-models.sort_values(by='Score', ascending=False).to_csv("results/prediction-score.csv")
+models.sort_values(by='Score', ascending=False).to_csv("results-without-weather/prediction-score.csv")
 
 
